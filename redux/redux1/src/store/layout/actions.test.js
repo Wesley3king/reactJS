bc36b2b57a2b4bbe42720d5b4d58aa6ea07953d6
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-18 13:43:24 -0300

    redux-toolkit implementado.

--- redux/redux1/src/store/layout/actions.test.js
+++ redux/redux1/src/store/layout/actions.test.js
@@ -1,24 +1,24 @@
 import React from "react"
-import { Types, Creators } from "./index"
+import { showMessage, hideMessage } from "./index"
 
 describe('Layout Actions', ()=> {
     describe('Actions Types', ()=> {
         test('deve mostrar a action type showMessage', ()=>{
-            expect(Types.showMessage).toEqual('showMessage');
+            expect(showMessage.type).toEqual('showMessage');
         });
 
         test('deve mostrar a action type hideMessage', ()=>{
-            expect(Types.hideMessage).toEqual('hideMessage');
+            expect(hideMessage.type).toEqual('hideMessage');
         });
     });
 
-    describe('Action creators', ()=> {
-        test('deve retornar o { type : showMessage }', ()=> {
-            expect(Creators.showMessage()).toEqual({type : Types.showMessage})
+    describe('Actions creators', ()=> {
+        test('deve retornar  a action creator showMessage', ()=>{
+            expect(showMessage()).toEqual({ type: 'showMessage'});
         });
 
-        test('deve retornar o { type : hideMessage }', ()=> {
-            expect(Creators.hideMessage()).toEqual({type : Types.hideMessage})
+        test('deve mostrar a action creator hideMessage', ()=>{
+            expect(hideMessage()).toEqual({type: 'hideMessage'});
         });
     });
 })
--- redux/redux1/src/store/layout/index.js
+++ redux/redux1/src/store/layout/index.js
@@ -1,47 +1,32 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
 const initialState = {
     showMessage: false
  };
 
-export default (state = initialState, action) => {
-    switch (action.type) {
-        case 'showMessage' : 
-        console.log("mostrar a menssagem");
-            return {...state, showMessage : true};
-        case 'hideMessage' :
-            return {...state, showMessage: false};
-        default : 
-            return state;
-    };
-};
-
 // Actions types
-export const Types = { 
+export const Types = {
     showMessage: "showMessage",
     hideMessage: "hideMessage"
  };
 //actions creators
-export const Creators = {
-    showMessage : () => {
-        return {
-            type : Types.showMessage
-        };
-    },
-    hideMessage : () => {
-        return {
-            type : Types.hideMessage
-        };
-    }
-};
+export const showMessage = createAction(Types.showMessage);
+export const hideMessage = createAction(Types.hideMessage);
 
-export const showMessage = () => {
-    console.log("foi");
-    return {
-        type : 'showMessage'
+//criar o reducer
+export default createReducer(initialState, {
+    [showMessage.type]: (state,action)=> ({...state, showMessage: true}),
+    [hideMessage.type]: (state,action)=> ({...state, showMessage: false})
+});
+/*
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case showMessage.type : 
+        console.log("mostrar a menssagem");
+            return {...state, showMessage : true};
+        case hideMessage.type :
+            return {...state, showMessage: false};
+        default : 
+            return state;
     };
-};
-
-export const hideMessage = () => {
-    return {
-        type : 'hideMessage'
-    };
-};
+};*/
